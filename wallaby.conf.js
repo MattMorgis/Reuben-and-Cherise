404128664b7173334093eda8cfaa85ec55641db0
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2018-01-20 00:50:51 -0500

    chore: removing flat text file reference

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -2,12 +2,7 @@ require('dotenv').config();
 
 module.exports = () => {
   return {
-    files: [
-      'src/**/*.js',
-      '!src/**/*.test.js',
-      '__mocks__/**/*.js',
-      'src/latest-instagram.txt',
-    ],
+    files: ['src/**/*.js', '!src/**/*.test.js', '__mocks__/**/*.js'],
 
     tests: ['src/**/*.test.js'],
 
