6e5c923badfca2309422e6602d27b7c03e905d93
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2018-01-19 23:01:56 -0500

    test: adding first failing test

--- src/main.test.js
+++ src/main.test.js
@@ -1,7 +1,10 @@
+// jest.dontMock('request');
+
 const {lamda} = require('./main');
 
-test('first test', () => {
+test('first test', done => {
   lamda(null, null, (error, caption) => {
-    expect(caption).toBe('Helllllllo, Lamda!');
-  })
+    expect(caption).toBe('Satya Nadella sitting on a bench');
+    done();
+  });
 });
